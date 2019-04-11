1b341ae031ac956664ac82c0ddb38e8eac723fd6
halented <hallie.dunn1@gmail.com>
2019-04-11 16:31:05 -0700

    added search & route

---
+++ app/controllers/search_controller.rb
@@ -0,0 +1,27 @@
+class SearchController < ApplicationController
+
+    def new
+        whatever = {}
+        twits = []
+        if params[negative_attitude]
+        $client.search("#{params[search_terms]} :(", result_type: "recent").take(5).collect do |tweet| 
+            twits << "#{tweet.url}"
+            end
+        elsif params[positive_attitude]
+            $client.search("#{params[search_terms]} :)", result_type: "recent").take(5).collect do |tweet| 
+            twits << "#{tweet.url}"
+            end
+        else 
+            $client.search("#{params[search_terms]}", result_type: "recent").take(5).collect do |tweet| 
+            twits << "#{tweet.url}"
+            end
+        end
+        
+        twits.map! do |tweetUrl|
+            HTTParty.get("https://publish.twitter.com/oembed?url="+tweetUrl)
+        end
+        whatever['tweet']=twits
+        render json: whatever
+    end
+
+end
